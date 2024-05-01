930a3e2f87cc4f0dbbf98437777ba623f8ab7097
sivakumar0703 <ssivakumar358@gmail.com>
2024-05-01 10:51:51 +0530

    adding_spinner

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,18 +1,19 @@
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import image from "../../assets/logo.png";
 import { useNavigate } from 'react-router-dom';
 import { myContext } from '../../context/Context';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { ClipLoader } from 'react-spinners';
 
 const Login = () => {
 
     const [loginCredentials,setLoginCredentials] = useState({email:"",password:""});
     const [toggle,setToogle] = useState(false);
     const passwordField = document.getElementById("loginPassword");
-    const [trigger,setTrigger] = useState(false);
+    const [loading , setLoading] = useState(false);
     const navigate = useNavigate();
     const {url} = useContext(myContext);
 
@@ -20,20 +21,31 @@ const Login = () => {
       setLoginCredentials({...loginCredentials ,[e.target.name]:e.target.value})
     }
 
-    async function login(e){
-        e?.preventDefault();
+    async function login(e,credentials){
+        e.preventDefault();
        try {
-        console.log('login',loginCredentials)
+        if(credentials){
+        setLoading(true);
+        let user = url && await axios.post(`${url}/user/login` , credentials);
+        sessionStorage.setItem("user",JSON.stringify(user.data.user));
+        toast.success(user.data.message.toUpperCase());
+        navigate('/home');
+        setLoading(false);
+        return
+        }
+        setLoading(true);
         const data = loginCredentials;
-        const user = url && await axios.post(`${url}/user/login` , data);
+        let user = url && await axios.post(`${url}/user/login` , data);
         sessionStorage.setItem("user",JSON.stringify(user.data.user));
         setLoginCredentials({email:"",password:""});
         toast.success(user.data.message.toUpperCase());
         navigate('/home');
+        setLoading(false);
        } catch (error) {
         error.response ?
         toast.error(error.response.data.message.toUpperCase()) :
-        toast.error(error.message)     
+        toast.error(error.message)  
+        setLoading(false);   
        }
     }
 
@@ -50,23 +62,6 @@ const Login = () => {
       navigate('/login/forgot_password');
     }
 
-     function quickLogin(data){
-      setLoginCredentials(data);
-      setTrigger(true);
-    }
-
-    useEffect(()=>{
-      if(trigger){
-        login()
-        setTrigger(false);
-      }
-    },[trigger])
-
-
-  
-
-  
-
   return (
     <div className='container login-container row'>
         <div id="logo-image" className='col-md-6 col-12'>
@@ -81,6 +76,15 @@ const Login = () => {
     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
 
+  {
+    loading ? <div style={{zIndex:"1000",position:"absolute",top:"50%",left:"50%"}}>
+      <ClipLoader
+      color="black"
+      size={50}
+    />
+    </div> : ""
+  }
+
   <label htmlFor="loginPassword" className="form-label my-text-size">Password</label>
   <div className="mb-3 input-group flex-nowrap"> <br/>
     <input type="password" className="form-control input-field" name="password" id="loginPassword" onChange={handleChange} /> 
@@ -94,13 +98,13 @@ const Login = () => {
   </div>
 
   <div className='text-center'>
-  <button type="submit" className="btn btn-primary" style={{textAlign:"center"}}>Submit</button>
+  <button type="submit" className="btn btn-primary" style={{textAlign:"center"}}>Login</button>
   </div>
 
    <div id="guest-login" style={{marginTop:"10px",textAlign:"center"}}>
-      <button className='btn btn-warning w-100 mb-1' onClick={() => quickLogin({email:"siva@xyz.com",
+      <button className='btn btn-warning w-100 mb-1' onClick={(e) => login(e,{email:"siva@xyz.com",
       password:"sivakumar"})}> Admin </button> <br/>
-      <button className='btn btn-info w-100' onClick={() => quickLogin({email:"guest@guest.com",
+      <button className='btn btn-info w-100' onClick={(e) => login(e,{email:"guest@guest.com",
       password:"sivakumar"})}> Guest </button>
    </div>
 </form>
